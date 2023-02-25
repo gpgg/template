5ed21a2a8053b43186a7cbe963f4a6206eb250e6
Zhongtao Miao <zhongtao.miao@gmail.com>
2023-02-26 01:16:13 +0900

    demo

--- template/demo.py
+++ template/demo.py
@@ -1,3 +1,6 @@
 import torch
 
-w = torch.ones((3, 1), dtype=torch.float, requires_grad=True, )
+device = 'cuda:0' if torch.cuda.is_available() else 'cpu'
+w = torch.ones((3, 1), dtype=torch.float, requires_grad=True, device=device)
+
+print(w)
